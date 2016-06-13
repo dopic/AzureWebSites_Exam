c81295474327904a5aa1fe5e5ccda866cd7821c2
douglas <douglas.picolotto@gmail.com>
2016-06-13 18:54:58 -0300

    Add project files.

---
+++ AzureWebSites_Exam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebSites_Exam.Startup))]
+namespace AzureWebSites_Exam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
